16722fed2a0bd0c48942e326b59b4b1f0475e0ca
al-dossari <jumanazarov00@bk.ru>
2024-02-19 12:52:04 +0500

    python shutil copy file to another directory

---
+++ main.py
@@ -0,0 +1,7 @@
+import os,shutil
+
+# if not os.path.exists('datatest'):
+#     os.makedirs('datatest')
+
+# shutil.copy('.env','C:\\Users\\Администратор\\Desktop\\smarts software')
+shutil.copy2('.env','C:\\Users\\Администратор\\Desktop\\smarts software')
